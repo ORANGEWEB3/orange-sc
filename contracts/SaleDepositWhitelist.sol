7941fc96f891abfe37603f9f5132d4cb7721fcbc
ssnar <undefined@udefined.com>
2024-05-09 13:34:06 +0700

    remove unused import

--- contracts/SaleDepositWhitelist.sol
+++ contracts/SaleDepositWhitelist.sol
@@ -1,6 +1,5 @@
 pragma solidity ^0.8.13;
 
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
